da77da1c2f9358de5ad5a7f6488cc388d9998b77
jyore <joey@jyore.com>
2013-09-23 10:33:55 -0500

    Added drawers and example, removed old activities and fragments

--- openshift-android/src/main/java/com/openshift/android/fragment/OnApplicationUpdateListener.java
+++
@@ -1,9 +0,0 @@
-package com.openshift.android.fragment;
-
-import com.openshift.android.model.ApplicationResource;
-
-public interface OnApplicationUpdateListener {
-	
-	public void onApplicationUpdate(ApplicationResource applicationResource);
-
-}
